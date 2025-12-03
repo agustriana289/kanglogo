2abf62366ac821bcc6a8458e9405372ebdec6232
NaiaProjects <naiaprojects9@gmail.com>
2025-12-04 01:22:59 +0700

    fix: Update 12/3/2025 - 08.22

--- components/SingleServicePricing.tsx
+++ components/SingleServicePricing.tsx
@@ -4,26 +4,11 @@ import LogoLoading from "./LogoLoading";
 
 export default function SingleServicePricing({
   service,
-  loading,
 }: {
-  service: Service | null;
-  loading: boolean;
+  service: Service;
 }) {
   if (!service) {
     return <div>Layanan tidak ditemukan.</div>;
-  }
-
-  if (loading) {
-    return (
-      <div className="fixed inset-0 bg-slate-100 dark:bg-slate-900 flex items-center justify-center z-50">
-        <div className="flex flex-col items-center justify-center">
-          <LogoLoading size="xl" />
-          <p className="mt-8 text-xl text-slate-600 dark:text-slate-400">
-            Sedang memuat...
-          </p>
-        </div>
-      </div>
-    );
   }
 
   return (
