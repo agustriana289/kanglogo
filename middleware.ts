4bcc61aba2edfc02f1aa7d7f4bc11f285d1770fe
NaiaProjects <naiaprojects9@gmail.com>
2025-12-04 08:46:26 +0700

    fix: Update 12/3/2025 - 08.22

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
 // middleware.ts
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
+  // Buat Supabase client
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 
+  // Dapatkan session user
   const {
     data: { session },
   } = await supabase.auth.getSession();
@@ -17,7 +21,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return res;
+  return NextResponse.next();
 }
 
 export const config = {
